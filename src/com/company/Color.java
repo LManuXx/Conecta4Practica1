f4f6421172fb85679aef5dc1f7c2bf23b2bdbf63
LManuXx <71410341+LManuXx@users.noreply.github.com>
2022-10-06 15:45:40 +0200

    Turn medio hecho, falta escribir el mensaje del gandor y el color activo

--- src/com/company/Color.java
+++ src/com/company/Color.java
@@ -6,4 +6,6 @@ enum Color {
     NULL;
 
 
+
+
 }
--- src/com/company/Turn.java
+++ src/com/company/Turn.java
@@ -1,5 +1,24 @@
 package com.company;
 
 public class Turn {
+    private Board board;
+    private Player[] players;
+    private int activePlayer;
+    static final int NUMBER_PLAYERS = 2;
+
+    Turn(Board board){
+        assert board != null;
+        this.board = board;
+        this.players = new Player[Turn.NUMBER_PLAYERS];
+        this.reset();
+    }
+
+    public void reset(){
+        for(int i = 0; i < NUMBER_PLAYERS; i++){
+            this.players[i] = new Player();
+        }
+        this.activePlayer = 0;
+    }
+
 
 }
